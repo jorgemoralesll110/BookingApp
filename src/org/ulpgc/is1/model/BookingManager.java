ef5bf58388c5f84fb97db14f4b2446f52f6e4d28
raquelaq <raquel.almeida105@alu.ulpgc.es>
2024-10-15 23:05:34 +0100

    Fouth Commit: fixed class BookingManager

--- src/org/ulpgc/is1/model/BookingManager.java
+++ src/org/ulpgc/is1/model/BookingManager.java
@@ -4,22 +4,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BookingManager {
-    private List<Customer> customers = new ArrayList<Customer>();
-    private List<Vehicle> vehicles = new ArrayList<Vehicle>();
-    private List<PromotionalCode> promotionalCodes = new ArrayList<PromotionalCode>();
-    private List<Member> members = new ArrayList<Member>();
-    private List<Booking> bookingList = new ArrayList<Booking>();
+    private List<Customer> customers;
+    private List<PromotionalCode> promotionalCodes;
+    private List<Vehicle> vehicles;
+    private List<Booking> bookings;
+
+    public BookingManager() {
+        this.customers = new ArrayList<>();
+        this.promotionalCodes = new ArrayList<>();
+        this.vehicles = new ArrayList<>();
+        this.bookings = new ArrayList<>();
+    }
 
     public void addCustomer(Customer customer) {
-        if (!customers.contains(customer)) {
-            customers.add(customer);
+        customers.add(customer);
+    }
+
+    public Customer getCustomer(int index) {
+        return customers.get(index);
+    }
+
+    public void addMember(Member member) {
+        customers.add(member);
+    }
+
+    public Member getMember(int index) {
+        Customer customer = customers.get(index);
+        if (customer instanceof Member) {
+            return (Member) customer;
         }
+        return null;
     }
 
     public void addVehicle(Vehicle vehicle) {
-        if (!vehicles.contains(vehicle)) {
-            vehicles.add(vehicle);
-        }
+        vehicles.add(vehicle);
     }
 
+    public Vehicle getVehicle(int index) {
+        return vehicles.get(index);
+    }
+
+    public void addPromotionalCode(PromotionalCode promoCode) {
+        promotionalCodes.add(promoCode);
+    }
+
+    public PromotionalCode getPromotionalCode(int index) {
+        return promotionalCodes.get(index);
+    }
+    public void book(Customer customer, Vehicle vehicle, PromotionalCode promoCode, Booking booking) {
+        booking.setPromotionalCode(promoCode);
+        bookings.add(booking);
+        customer.addBooking(booking);
+        vehicle.addBooking(booking);
+    }
+
+    public List<Booking> getBookingList() {
+        return bookings;
+    }
 }
